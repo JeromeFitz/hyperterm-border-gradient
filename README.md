29b32f797b0168a0c99c51b00953436ea7212d52
JeromeFitz <jeromes@gmail.com>
2016-08-02 15:09:57 -0400

    Custom Plugin for HyperTerm border-color gradient :ghost:
    This is a test for the workflow and configuration options for HyperTerm.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gradient Border for HyperTerm
+
+## Props
+https://github.com/webmatze/hyperborder/
+
+As I was looking into the CSS Elements for HyperTerm, I found their Repo.
+It is basically way better and as a result mine is strictly a `~/.hyperterm_plugins/local/` version.
+
+## HyperTerm
+https://hyperterm.org/
---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports.decorateConfig = (config) => {
+
+  const tempConfig    = Object.assign({
+    borderColors: ['#FCFF0C', '#F81CE5'],
+    borderWidth:  '3px'
+  }, config.hypertermBorderGradient);
+  const colorGradient = tempConfig.borderColors.join(',');
+  const borderWidth   = tempConfig.borderWidth;
+
+  return Object.assign({}, config, {
+    css: `
+      html {
+        background-image: linear-gradient(${colorGradient});
+        border-radius:    ${borderWidth};
+        height:           100%;
+      }
+      body {
+        border-radius: ${borderWidth};
+        bottom:        ${borderWidth};
+        left:          ${borderWidth};
+        position:      absolute;
+        right:         ${borderWidth};
+        top:           ${borderWidth};
+      }
+      ${config.css || ''}
+      #mount {
+      }
+      .hyperterm_main {
+        border-width: 0px;
+        bottom:       ${borderWidth};
+        left:         ${borderWidth};
+        right:        ${borderWidth};
+        top:          ${borderWidth};
+      }
+      .hyperterm_main .header_header {
+        left:  ${borderWidth};
+        right: ${borderWidth};
+        top:  ${borderWidth};
+      }
+      .hyperterm_main .tabs_list {
+        border-bottom-color:     ${config.borderColor};
+        border-top-left-radius:  ${borderWidth};
+        border-top-right-radius: ${borderWidth};
+      }
+      .hyperterm_main .tab_tab:last-child {
+        border-top-right-radius: ${borderWidth}
+      }
+      .hyperterm_main .terms_terms {
+        border-radius: 0 0 ${borderWidth} ${borderWidth};
+        bottom:        ${borderWidth};
+        left:          ${borderWidth};
+        right:         ${borderWidth};
+      }
+      .hyperterm_main .terms_term {
+        margin-top: ${borderWidth};
+      }
+    `
+  });
+}
